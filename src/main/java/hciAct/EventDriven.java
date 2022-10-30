035dcdca925f5e5c6b054dc142f22c56c7c1b117
pitzzahh <pitzzahh@gmail.com>
2022-10-30 23:03:37 +0800

    add some inner classes

--- src/main/java/hciAct/EventDriven.java
+++ src/main/java/hciAct/EventDriven.java
@@ -1,6 +1,8 @@
 package hciAct;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class EventDriven extends JFrame {
     // create a JFrame that ask for user input
@@ -11,6 +13,33 @@ public class EventDriven extends JFrame {
         frame.setSize(300, 300);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    static class ButtonSubmit implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            System.out.println("Button submit is clicked");
+        }
+    }
+
+    static class ButtonClearAll implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            System.out.println("Clear all button is clicked");
+        }
+    }
+
+    static class ButtonOkay implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            System.out.println("Okay button is clicked");
+        }
     }
 
     public static void main(String[] args) {
