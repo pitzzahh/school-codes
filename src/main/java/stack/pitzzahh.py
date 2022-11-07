7ac6990645f69d6facfce02c9f6fd4c44dc66fa6
pitzzahh <pitzzahh@gmail.com>
2022-11-07 23:27:20 +0800

    quick update

--- src/main/java/stack/pitzzahh.py
+++ src/main/java/stack/pitzzahh.py
@@ -1,8 +1,21 @@
 #
 # written by Peter John Arao
 # using recursive functions
-#
-
+# ha?? sino sino connected??
+# hahahahah hi ms.clemena
+# lol . magdidiscuss tayo bukas about sa 3 topics sa dsa
+# programmer ka ms.izyl??
+# hindi paman po ako magaling HAHAH
+# pero marunong?
+# hahahah humble
+# magchecheck pa ako mga activities nyo hahahahaha
+#annyeong
+# byee pu
+# slight po? hahahah
+# sige po ma'am byee po
+# good night poo
+# good evening po ma'am
+# si Izyl po tapos si Adrian kanina umalis na
 def main():
     basket = []
     fruits = ('Apple', 'Orange', 'Mango', 'Guava')
