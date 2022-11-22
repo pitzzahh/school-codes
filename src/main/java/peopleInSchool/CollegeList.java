84b8947fc28fe2aea65aa68c39e883e017b2fda9
pitzzahh <araopj2191@gmail.com>
2022-11-22 09:28:51 +0800

    quick upload

--- src/main/java/peopleInSchool/CollegeList.java
+++ src/main/java/peopleInSchool/CollegeList.java
@@ -1,6 +1,9 @@
 package peopleInSchool;
 
+import io.github.pitzzahh.util.utilities.validation.Validator;
+
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class CollegeList {
 
@@ -12,6 +15,35 @@ public class CollegeList {
     private void askUser(Scanner scanner) {
         System.out.print("Press E for Employee, F for Faculty, S for Student, or Q to quit: ");
         String input = scanner.nextLine();
+        if (input.trim().equalsIgnoreCase("E")) {
+            employee(scanner);
+        }
+
     }
 
+    /**
+     * Employee input
+     * @param scanner the scanner object.
+     */
+    private void employee(Scanner scanner) {
+        System.out.print("Enter employee name          : ");
+        String name = scanner.nextLine().trim();
+        System.out.print("Enter employee contact number: ");
+        String contactNumber = scanner.nextLine().trim();
+        System.out.print("Enter employee salary        : ");
+        String salary = scanner.nextLine().trim();
+        System.out.print("Enter employee department    : ");
+        String department = scanner.nextLine().trim();
+
+        Employee employee = new Employee();
+
+        employee.setName(name);
+        employee.setContactNumber(contactNumber);
+
+        Pattern pattern = Pattern.compile("^(\\d+\\.\\d+|\\.\\d+|0\\.\\d+)$");
+        boolean isDouble = pattern.matcher(salary).matches();
+
+        if (isDouble) employee.setSalary(Double.parseDouble(salary));
+
+    }
 }
