48e99fd7717251be8954412e21d52ff1cbc82162
pitzzahh <araopj2191@gmail.com>
2022-11-25 16:16:46 +0800

    add scratch file

---
+++ src/main/java/scratch/App.java
@@ -0,0 +1,17 @@
+package scratch;
+
+import java.util.PriorityQueue;
+
+public class App {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(4);
+        queue.add(2);
+        queue.add(1);
+        queue.add(3);
+        queue.add(5);
+        queue.stream()
+             .sorted()
+             .forEach(System.out::println);
+    }
+}
